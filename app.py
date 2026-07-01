b28380b80470ddda89cfe7c1828729c9f7397b82
Anusree A <anusreeanil1810@gmail.com>
2026-07-01 21:43:36 +0530

    dashboard

--- app.py
+++ app.py
@@ -1,45 +1,14 @@
-from flask import Flask
-import psycopg2
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Connect to ShaktiDB
-conn = psycopg2.connect(
-    dbname="youtube_analytics",
-    user="anusree",
-    host="/tmp"
-)
-@app.route("/")
+@app.route('/')
 def home():
-    cur = conn.cursor()
+    return render_template('index.html')
 
-    cur.execute("""
-        SELECT video_title, views
-        FROM video
-        JOIN analytics
-        ON video.video_id = analytics.video_id
-        ORDER BY views DESC;
-    """)
-
-    videos = cur.fetchall()
-
-    cur.close()
-
-    html = """
-    <h1>YouTube Channel Analytics</h1>
-    <table border="1" cellpadding="10">
-        <tr>
-            <th>Video</th>
-            <th>Views</th>
-        </tr>
-    """
-
-    for video in videos:
-        html += f"<tr><td>{video[0]}</td><td>{video[1]}</td></tr>"
-
-    html += "</table>"
-
-    return html
+@app.route('/dashboard', methods=['POST'])
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
